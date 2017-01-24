3277aa2523f9d8731e42cac2618c61d36f46a2bc
Labeeb Ibrahim <labeeb@hamon.in>
2017-01-24 17:26:20 +0530

    completes Excercise 1

---
+++ lcthw/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* This is a comment */
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+  // This is also a comment
+
+  printf("You are %d miles away. \n", distance);
+  printf("You couild go to %s from here. \n", "Washington");
+  printf("You could take a bus or a plane.\n");
+  printf("But it would be funny to take a plabe that leaves after %d hrs. \n", 24);
+  printf("Remember, the distance is just %d miles. Good luck planning.  \n", distance);
+  
+  return 0;
+}
