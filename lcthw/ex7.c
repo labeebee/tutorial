83e33b9a53e1fdc80545ee90b24f05f5fe32c45c
Labeeb Ibrahim <labeeb@hamon.in>
2017-01-25 10:55:29 +0530

    finishes ex7

--- lcthw/ex7.c
+++ lcthw/ex7.c
@@ -26,4 +26,18 @@ int main(int argc, char* argv[])
   long universe_of_defects = 1L *1024* 1024L *1024L;
   printf("The entire universe has %ld bugs", universe_of_defects);
   double expected_bugs = bugs * bug_rate;
-  double part_of_universe = expect
+  printf("You are expected to have %f bugs. \n", expected_bugs);
+
+  double part_of_universe = expected_bugs / universe_of_defects;
+  printf("That is only a %e portion of the universe. \n",part_of_universe);
+
+  //This makes no sense, just a demo of something weird
+
+  char nul_byte = '\0';
+  int care_percentage = bugs * nul_byte;
+  printf("The null byte is %c\n", nul_byte);
+  printf("Which means you should care %d%%. \n", care_percentage);
+
+  return 0;
+}
+
